8f2793153afc4594bcebfef6e67327a2cebc5bda
jc <jchorzempa@posteo.org>
2022-07-20 09:29:09 +0200

    Add more pseudocode

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,11 @@
 // Write the function getComputerChoice that generates a random outcome.
-
-// The function enables the Computer to choose between Rock, Paper and Scissors and mak,e a random selection.
-
+// The function enables the Computer to choose between Rock, Paper and Scissors and make a random selection.
 //The only choices are the three above, no others.
 
+function getComputerChoice() = Random choice between {Rock, Paper, Scissors}
+
+
+
 // The Computer's Choice is printed into the JavaScript console to view.
+
+console.log(getComputerChoice)
