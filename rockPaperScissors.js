112e8e269ca630995cbc8db5be8b1593d8656b6c
jc <jchorzempa@posteo.org>
2022-07-20 09:24:08 +0200

    Add pseudocode for first task

---
+++ rockPaperScissors.js
@@ -0,0 +1,7 @@
+// Write the function getComputerChoice that generates a random outcome.
+
+// The function enables the Computer to choose between Rock, Paper and Scissors and mak,e a random selection.
+
+//The only choices are the three above, no others.
+
+// The Computer's Choice is printed into the JavaScript console to view.
