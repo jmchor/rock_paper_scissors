755b8fce4c558bdc65f75f919eb4183e3bb4fa23
jc <jchorzempa@posteo.org>
2022-07-21 07:24:09 +0200

    Add pseudocode

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,8 +1,24 @@
 // Write the function getComputerChoice that generates a random outcome.
-// The function enables the Computer to choose between Rock, Paper and Scissors and make a random selection.
-//The only choices are the three above, no others.
 
 function getComputerChoice() = Random choice between {Rock, Paper, Scissors}
+
+
+
+//The only choices are the three above, no others. They are always the same and constant.
+
+const choices = ["Rock", "Paper", "Scissors"]
+
+
+
+
+// The function enables the Computer to choose between Rock, Paper and Scissors and make a random selection.
+
+Random choice between choice 1, choice 2 and choice 3
+
+// The choice is between 1, 2 and 3, so get a random number from all numbers in the choices group
+
+random number (number of items in group)
+
 
 
 
