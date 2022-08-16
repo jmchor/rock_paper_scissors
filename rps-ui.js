c85715e16be53f66147a3772749c1667506bb5cc
jc <jchorzempa@posteo.org>
2022-08-16 15:25:33 +0200

    Edit code to end game after reaching five points

--- rps-ui.js
+++ rps-ui.js
@@ -1,3 +1,9 @@
+let announceBox = document.querySelector('.outcome');
+
+const replay = document.querySelector('.reload');
+
+const buttonBox = document.querySelector('.buttonBox');
+
 function computerHand() {
 	let choices = ['rock', 'paper', 'scissors'];
 	let computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -5,31 +11,46 @@ function computerHand() {
 	return computerChoice;
 }
 
-function playerHand(i) {
-	let playerChoice = prompt(`This is round ${i}. Rock, Paper or Scissors?`);
-	return playerChoice.toLowerCase();
+const buttons = document.querySelectorAll('button');
+
+let playerScore = 0;
+let computerScore = 0;
+
+function allTheButtons(button) {
+	button.addEventListener('click', () => {
+		let playerSelection = button.id;
+		let computerSelection = computerHand();
+
+		let para = document.createElement('p');
+
+		if (playerSelection === computerSelection) {
+			para.textContent = `The Computer played ${computerSelection}. It's a tie!`;
+		} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+			computerScore++;
+			para.textContent = `The Computer played ${computerSelection}. Computer Wins! ${playerScore} to ${computerScore}`;
+		} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+			computerScore++;
+			para.textContent = `The Computer played ${computerSelection}. The Computer Wins! (No argument there.) ${playerScore} to ${computerScore}`;
+		} else if (!['rock', 'paper', 'scissors'].includes(playerSelection)) {
+			window.location.reload(true);
+		} else {
+			playerScore++;
+			para.textContent = `The Computer played ${computerSelection}. The Player Wins! No Terminator Scenario Approaching (for now)! ${playerScore} to ${computerScore}`;
+		}
+
+		if (announceBox.hasChildNodes()) {
+			announceBox.removeChild(announceBox.childNodes[0]);
+			announceBox.appendChild(para);
+		} else {
+			announceBox.appendChild(para);
+		}
+
+		if (playerScore === 5 || computerScore === 5) {
+			buttonBox.classList.add('invisible');
+			replay.classList.remove('invisible');
+			para.textContent = `End of Game! Player: ${playerScore}, Computer ${computerScore}`;
+		}
+	});
 }
 
-function playRound(playerSelection, computerSelection) {
-	let result = computerSelection[0].toUpperCase() + computerSelection.substring(1);
-
-	if (playerSelection === computerSelection) {
-		console.log(`The Computer played ${result}. It's a tie!`);
-	} else if (playerSelection === 'rock' && computerSelection === 'paper') {
-		computerScore++;
-		console.log(
-			`The Computer played ${result}. The Computer Wins for some reason! (I mean, sure, paper envelops rock, but ever hear of a paper weight? seems like a lose-lose-kinda thing)`
-		);
-	} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-		computerScore++;
-		console.log(`The Computer played ${result}. The Computer Wins! (What if it's a whetstone though...)`);
-	} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-		computerScore++;
-		console.log(`The Computer played ${result}. The Computer Wins! (No argument there.)`);
-	} else if (!['rock', 'paper', 'scissors'].includes(playerSelection)) {
-		console.log('No valid input!');
-	} else {
-		playerScore++;
-		console.log(`The Computer played ${result}. The Player Wins! No Terminator Scenario Approaching (for now)!`);
-	}
-}
+buttons.forEach(allTheButtons);
