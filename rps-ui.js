42ccd54fe833e869a095b92a5a7e457a1aa71e19
jc <jchorzempa@posteo.org>
2022-09-12 10:25:29 -0500

    Add scoreboard JS code

--- rps-ui.js
+++ rps-ui.js
@@ -1,4 +1,5 @@
 let announceBox = document.querySelector('.outcome');
+let scoreBox = document;
 
 const replay = document.querySelector('.reload');
 
@@ -22,26 +23,43 @@ function allTheButtons(button) {
 		let computerSelection = computerHand();
 
 		let para = document.createElement('p');
+		let playerPoints = document.querySelector('#playerscore');
+		let machineScore = document.querySelector('#machinescore');
 
 		if (playerSelection === computerSelection) {
 			para.textContent = `You chose ${playerSelection.toUpperCase()}. The Computer played ${computerSelection.toUpperCase()}. It's a tie!`;
 		} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
 			computerScore++;
-			para.textContent = `You chose ${playerSelection.toUpperCase()}. The Computer played ${computerSelection.toUpperCase()}. Computer Wins! ${playerScore} to ${computerScore}`;
+			para.textContent = `You chose ${playerSelection.toUpperCase()}. The Computer played ${computerSelection.toUpperCase()}. Computer Wins!`;
+			playerPoints.textContent = `${playerScore}`;
+			machineScore.textContent = `${computerScore}`;
 		} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
 			computerScore++;
-			para.textContent = `You chose ${playerSelection.toUpperCase()}. The Computer played ${computerSelection.toUpperCase()}. The Computer Wins! (No argument there.) ${playerScore} to ${computerScore.toUpperCase()}`;
+			para.textContent = `You chose ${playerSelection.toUpperCase()}. The Computer played ${computerSelection.toUpperCase()}. The Computer Wins! (No argument there.)`;
+			playerPoints.textContent = `${playerScore}`;
+			machineScore.textContent = `${computerScore}`;
 		} else if (playerSelection === 'rock' && computerSelection === 'paper') {
 			computerScore++;
-			para.textContent = `You chose ${playerSelection.toUpperCase()}. The Computer played ${computerSelection.toUpperCase()}. The Computer Wins! (No argument there.) ${playerScore} to ${computerScore}`;
+			para.textContent = `You chose ${playerSelection.toUpperCase()}. The Computer played ${computerSelection.toUpperCase()}. The Computer Wins! (No argument there.)`;
+			playerPoints.textContent = `${playerScore}`;
+			machineScore.textContent = `${computerScore}`;
 		} else if (!['rock', 'paper', 'scissors'].includes(playerSelection)) {
 			window.location.reload(true);
 		} else {
 			playerScore++;
-			para.textContent = `You chose ${playerSelection.toUpperCase()}. The Computer played ${computerSelection.toUpperCase()}. The Player Wins! No Terminator Scenario Approaching (for now)! ${playerScore} to ${computerScore}`;
+			para.textContent = `You chose ${playerSelection.toUpperCase()}. The Computer played ${computerSelection.toUpperCase()}. The Player Wins! No Terminator Scenario Approaching (for now)!`;
+			playerPoints.textContent = `${playerScore}`;
+			machineScore.textContent = `${computerScore}`;
 		}
 
 		if (announceBox.hasChildNodes()) {
+			announceBox.removeChild(announceBox.childNodes[0]);
+			announceBox.appendChild(para);
+		} else {
+			announceBox.appendChild(para);
+		}
+
+		if (scoreBox.hasChildNodes()) {
 			announceBox.removeChild(announceBox.childNodes[0]);
 			announceBox.appendChild(para);
 		} else {
